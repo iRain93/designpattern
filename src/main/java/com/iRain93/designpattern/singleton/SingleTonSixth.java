1d184b88361cabe8c925772805093822d0e6de37
iRain93 <1085594562@qq.com>
2020-02-07 04:03:42 +0800

    Create SingleTonSixth.java

---
+++ src/main/java/com/iRain93/designpattern/singleton/SingleTonSixth.java
@@ -0,0 +1,32 @@
+package com.iRain93.designpattern.singleton;
+
+/**
+ * 枚举类型实现单例模式
+ * @author lu
+ * @version 1.0
+ */
+public class SingleTonSixth {
+
+    private SingleTonSixth(){
+
+    }
+
+    public static SingleTonSixth getInstance(){
+        return SingleTonEnum.INSTANCE.getInstance();
+    }
+
+    private static enum SingleTonEnum{
+        INSTANCE;
+
+        private  SingleTonSixth singleTonSixth;
+
+        private  SingleTonEnum(){
+            singleTonSixth=new SingleTonSixth();
+        }
+
+        private  SingleTonSixth getInstance(){
+            return singleTonSixth;
+        }
+
+    }
+}
