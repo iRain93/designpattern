b74a3957e1b32b56376b1cf6d95d013864483266
iRain93 <1085594562@qq.com>
2020-02-16 23:08:02 +0800

    Update LazySingleTonSafe.java

--- src/main/java/com/iRain93/designpattern/singleton/LazySingleTonSafe.java
+++ src/main/java/com/iRain93/designpattern/singleton/LazySingleTonSafe.java
@@ -6,15 +6,15 @@ package com.iRain93.designpattern.singleton;
  * @version 1.0
  */
 public class LazySingleTonSafe {
-
+    /** 1.只有一个实例对象 */
     private static LazySingleTonSafe instance;
-
+    /** 2.自我实例化 */
     private LazySingleTonSafe() {
         if (instance != null) {
             throw new IllegalStateException("实例已经初始化！");
         }
     }
-
+    /** 3.提供全局访问点 */
     public static synchronized LazySingleTonSafe getInstance() {
         if (instance == null) {
             instance = new LazySingleTonSafe();
