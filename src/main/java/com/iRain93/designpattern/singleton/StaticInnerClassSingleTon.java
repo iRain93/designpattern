747886d0c3f75594d7523662a8912611ca8ab6e9
iRain93 <1085594562@qq.com>
2020-02-17 01:54:32 +0800

    Update StaticInnerClassSingleTon.java

--- src/main/java/com/iRain93/designpattern/singleton/StaticInnerClassSingleTon.java
+++ src/main/java/com/iRain93/designpattern/singleton/StaticInnerClassSingleTon.java
@@ -7,15 +7,14 @@ package com.iRain93.designpattern.singleton;
  */
 public class StaticInnerClassSingleTon {
 
-    private StaticInnerClassSingleTon() {
-    }
-
+    /** 2.自我实例化 */
+    private StaticInnerClassSingleTon() { }
+    /** 3.提供全局访问点 */
     public static StaticInnerClassSingleTon getInstance() {
         return StaticInnerClassSingleTonHolder.instance;
     }
-
+    /** 1.只有一个实例对象 */
     private static class StaticInnerClassSingleTonHolder {
         private static final StaticInnerClassSingleTon instance = new StaticInnerClassSingleTon();
     }
-
 }
