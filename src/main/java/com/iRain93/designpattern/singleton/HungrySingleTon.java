7996d87edff06840bdf83f29baea1f03fef3aed1
iRain93 <1085594562@qq.com>
2020-02-17 01:54:23 +0800

    Update HungrySingleTon.java

--- src/main/java/com/iRain93/designpattern/singleton/HungrySingleTon.java
+++ src/main/java/com/iRain93/designpattern/singleton/HungrySingleTon.java
@@ -7,14 +7,15 @@ package com.iRain93.designpattern.singleton;
  */
 public class HungrySingleTon {
 
+    /** 1.只有一个实例对象 */
     private static final HungrySingleTon instance = new HungrySingleTon();
-
+    /** 2.自我实例化 */
     private HungrySingleTon() {
         if (instance != null) {
             throw new IllegalStateException("实例已经初始化！");
         }
     }
-
+    /** 3.提供全局访问点 */
     public static HungrySingleTon getInstance() {
         return instance;
     }
