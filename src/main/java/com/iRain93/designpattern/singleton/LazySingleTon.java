63be7b018e48e56b44983a165ae5dc2e7f9a325c
iRain93 <1085594562@qq.com>
2020-02-16 23:08:05 +0800

    Update LazySingleTon.java

--- src/main/java/com/iRain93/designpattern/singleton/LazySingleTon.java
+++ src/main/java/com/iRain93/designpattern/singleton/LazySingleTon.java
@@ -6,14 +6,17 @@ package com.iRain93.designpattern.singleton;
  * @version 1.0
  */
 public class LazySingleTon {
+
+    /** 1.只有一个实例对象 */
     private static LazySingleTon instance;
 
+    /** 2.自我实例化 */
     private LazySingleTon() {
         if (instance != null) {
             throw new IllegalStateException("实例已经初始化！");
         }
     }
-
+    /** 3.提供全局访问点 */
     public static LazySingleTon getInstance() {
         if (instance == null) {
             instance = new LazySingleTon();
