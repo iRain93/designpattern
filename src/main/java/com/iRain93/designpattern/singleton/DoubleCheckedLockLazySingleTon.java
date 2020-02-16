ec371589f20a5b13957fbc9138c4079b7f561563
iRain93 <1085594562@qq.com>
2020-02-17 01:54:20 +0800

    Update DoubleCheckedLockLazySingleTon.java

--- src/main/java/com/iRain93/designpattern/singleton/DoubleCheckedLockLazySingleTon.java
+++ src/main/java/com/iRain93/designpattern/singleton/DoubleCheckedLockLazySingleTon.java
@@ -6,14 +6,15 @@ package com.iRain93.designpattern.singleton;
  * @version 1.0
  */
 public class DoubleCheckedLockLazySingleTon {
+    /** 1.只有一个实例对象 */
     private static volatile DoubleCheckedLockLazySingleTon instance;
-
+    /** 2.自我实例化 */
     private DoubleCheckedLockLazySingleTon() {
         if (instance != null) {
             throw new IllegalStateException("实例已经初始化！");
         }
     }
-
+    /** 3.提供全局访问点 */
     public static DoubleCheckedLockLazySingleTon getInstance() {
         if (instance == null) {
             synchronized (DoubleCheckedLockLazySingleTon.class) {
