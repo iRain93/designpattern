01039438e326436ea26a290203c43b9de326e1c5
iRain93 <1085594562@qq.com>
2020-03-26 17:43:31 +0800

    Create ShapeCache.java

---
+++ src/main/java/com/iRain93/designpattern/prototype/ShapeCache.java
@@ -0,0 +1,35 @@
+package com.iRain93.designpattern.prototype;
+
+import java.util.Hashtable;
+
+/**
+ * 图形缓存
+ * @author lu
+ * @version 1.0
+ */
+public class ShapeCache {
+
+    private static Hashtable<String, Shape> shapeMap = new Hashtable<String, Shape>();
+
+    public static Shape getShape(String shapeId) {
+        Shape cachedShape = shapeMap.get(shapeId);
+        return (Shape) cachedShape.clone();
+    }
+
+    /**
+     * 管理原型对象方法
+     */
+    public static void loadCache() {
+        Circle circle = new Circle();
+        circle.setId("1");
+        shapeMap.put(circle.getId(),circle);
+
+        Square square = new Square();
+        square.setId("2");
+        shapeMap.put(square.getId(),square);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setId("3");
+        shapeMap.put(rectangle.getId(),rectangle);
+    }
+}
