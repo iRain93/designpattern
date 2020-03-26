f25236af53e202ff59f64c7132144af71bbdc916
iRain93 <1085594562@qq.com>
2020-03-26 17:43:14 +0800

    Create Client.java

---
+++ src/main/java/com/iRain93/designpattern/prototype/Client.java
@@ -0,0 +1,22 @@
+package com.iRain93.designpattern.prototype;
+
+/**
+ * 用一句话描述次类
+ *
+ * @author lu
+ * @version 1.0
+ */
+public class Client {
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+
+        Shape clonedShape = (Shape) ShapeCache.getShape("1");
+        System.out.println("Shape : " + clonedShape.getType());
+
+        Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+        System.out.println("Shape : " + clonedShape2.getType());
+
+        Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
+        System.out.println("Shape : " + clonedShape3.getType());
+    }
+}
