2f9a1384867b76e2707f54f4bef0548756d1cc73
iRain93 <1085594562@qq.com>
2020-03-26 17:42:49 +0800

    Create Shape.java

---
+++ src/main/java/com/iRain93/designpattern/abstractfactory/Shape.java
@@ -0,0 +1,14 @@
+package com.iRain93.designpattern.abstractfactory;
+
+/**
+ * 形状接口
+ * @author lu
+ * @version 1.0
+ */
+public interface Shape {
+    /**
+     * 画
+     * @return void
+     */
+    void draw();
+}
