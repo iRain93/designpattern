90383c1f7b752d4e2abc53fa607a1bc846ad9c47
iRain93 <1085594562@qq.com>
2020-03-26 17:43:49 +0800

    Create Client.java

---
+++ src/main/java/com/iRain93/designpattern/template/Client.java
@@ -0,0 +1,13 @@
+package com.iRain93.designpattern.template;
+
+/**
+ * 客户端
+ * @author lu
+ * @version 1.0
+ */
+public class Client {
+    public static void main(String[] args) {
+        SexyBeauty girlA = new SexyBeautyA();
+        girlA.oneDay();
+    }
+}
