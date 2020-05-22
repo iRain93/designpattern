bab9bfe8ac8409644702ceb8912fae3f39e7d781
iRain93 <1085594562@qq.com>
2020-05-22 21:16:42 +0800

    Create Client.java

---
+++ src/main/java/com/iRain93/designpattern/dynamicproxy/cglib/Client.java
@@ -0,0 +1,16 @@
+package com.iRain93.designpattern.dynamicproxy.cglib;
+
+import com.iRain93.designpattern.dynamicproxy.SelenaGomez;
+import com.iRain93.designpattern.dynamicproxy.Star;
+import com.iRain93.designpattern.dynamicproxy.jdkproxy.JdkProxy;
+
+/**
+ * 舞台
+ * @author lu
+ * @version 1.0
+ */
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
