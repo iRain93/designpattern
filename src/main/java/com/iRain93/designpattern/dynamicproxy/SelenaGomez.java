a27988faa621f923ea524080af20f79c9df07e0c
iRain93 <1085594562@qq.com>
2020-05-22 21:17:17 +0800

    Create SelenaGomez.java

---
+++ src/main/java/com/iRain93/designpattern/dynamicproxy/SelenaGomez.java
@@ -0,0 +1,14 @@
+package com.iRain93.designpattern.dynamicproxy;
+
+/**
+ * Selena Gomez
+ * @author lu
+ * @version 1.0
+ */
+public class SelenaGomez implements Star {
+
+    @Override
+    public void sing() {
+        System.out.println("Selena Gomez::Sing a song");
+    }
+}
